b1667c713b1835c32c1983df9a5241a88aa44688
Aviralansh <avirald204@gmail.com>
2026-06-30 22:54:03 +0530

    Fix screen recorder: Use wf-recorder instead of gpu-screen-recorder

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ sudo apt install -y -t trixie-backports \
   wl-clipboard cliphist curl git trash-cli jq lazygit bat ripgrep ydotool \
   xdg-user-dirs brightnessctl power-profiles-daemon ddcutil swappy \
   fonts-noto fonts-noto-cjk fonts-noto-color-emoji unzip meson sassc starship fuzzel hyprpicker \
-  hyprland xdg-desktop-portal-hyprland python3-pip python3-venv hyprpaper hyprland-guiutils \
+  hyprland xdg-desktop-portal-hyprland python3-pip python3-venv hyprpaper hyprland-guiutils wf-recorder slurp \
   extra-cmake-modules libkf6colorscheme-dev libkf6config-dev libkf6iconthemes-dev \
   qml6-module-qt5compat-graphicaleffects qml6-module-qtquick-effects easyeffects
 
@@ -128,6 +128,7 @@ fi
 cd "$WORK_DIR/caelestia-cli-git"
 git reset --hard
 git apply "$SCRIPT_DIR/patches/caelestia-cli.patch"
+git apply "$SCRIPT_DIR/patches/caelestia-cli-recorder.patch"
 pip install --break-system-packages --user .
 
 # 8. Build and Install qtengine from source (with Debian Patches)
